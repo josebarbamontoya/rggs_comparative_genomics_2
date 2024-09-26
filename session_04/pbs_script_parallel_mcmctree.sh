2aad3abd0c869e0653a18b27d365ce7581f2145d
Jose Barba-Montoya <38330595+josebarbamontoya@users.noreply.github.com>
2024-09-26 12:51:02 -0400

    Add files via upload

---
+++ session_04/pbs_script_parallel_mcmctree.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#PBS -V
+#PBS -N mcmctree_parallel
+#PBS -q batch
+#PBS -S /bin/bash
+#PBS -m abe
+#PBS -M jbarba@amnh.org
+#PBS -e /home/jbarba/HPL_RUNS/error_file_name.txt
+#PBS -o /home/jbarba/HPL_RUNS/output_file_name.txt
+#PBS -l ncpus=5
+#PBS -l mem=10G
+#PBS -l walltime=48:00:00
+cd $PBS_O_WORKDIR
+echo Working directory is $PBS_O_WORKDIR
+
+### my code lines
+cd /home/jbarba/nas4/canid_evolution/canid_mtdna_mcmctree_huxley/mcmc01
+~/bin/mcmctree mcmctree.ctl > screen.txt &
+
+cd /home/jbarba/nas4/canid_evolution/canid_mtdna_mcmctree_huxley/mcmc02
+~/bin/mcmctree mcmctree.ctl > screen.txt &
+
+cd /home/jbarba/nas4/canid_evolution/canid_mtdna_mcmctree_huxley/mcmc03
+~/bin/mcmctree mcmctree.ctl > screen.txt &
+
+cd /home/jbarba/nas4/canid_evolution/canid_mtdna_mcmctree_huxley/mcmc04
+~/bin/mcmctree mcmctree.ctl > screen.txt &
+
+wait
