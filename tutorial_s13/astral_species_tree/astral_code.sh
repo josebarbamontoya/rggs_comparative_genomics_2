f44e1029e8252c9e3677403cd4b3f919fbba6420
Jose Barba-Montoya <38330595+josebarbamontoya@users.noreply.github.com>
2024-12-06 11:03:29 -0500

    Delete tutorial_s13 directory

--- tutorial_s13/astral_species_tree/astral_code.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#change dir
-cd /Users/barba/Desktop/phylogenetic_inference_tutorial/astral_species_tree
-
-#run astral with local posterior prob (lpp)
-java -jar astral.5.7.8.jar -i 100_ml_tres.nwk -o out.nwk 2>out.log
